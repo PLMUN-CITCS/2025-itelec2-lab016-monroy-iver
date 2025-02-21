aff7f2db4e3d6f4634b8fe5620d2d943dc3c55d5
monroy-iver <monroyiverjohn_bsit@plmun.edu.ph>
2025-02-20 23:59:57 -0800

    Submitting Python Week 04 - Laboratory # 16

---
+++ basic_function.py
@@ -0,0 +1,11 @@
+# Iver John R Monroy
+# ITELEC2
+# Laboratory #16 - Guided Coding Exercise : Basic Function Definition and Calling
+
+def greet():
+    """Prints a greeting message."""  # Docstring
+
+    print("Hello, Python Students!")  # Function body
+
+
+greet()
